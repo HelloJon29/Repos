00957f328880f89f76e8b2888032ca0f1e991834
HelloJon29 <jonkim1996@gmail.com>
2021-11-25 21:06:32 -0600

    Renamed java class

--- replacementwindowcalc/src/main/java/com/mycompany/replacementwindowcalc/WindowMaster.java
+++ replacementwindowcalc/src/main/java/com/mycompany/replacementwindowcalc/WindowMaster.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Calculator for Replacement Windows
  *
  */
-public class NewClass {
+public class WindowMaster {
 
     public static void main(String[] args) {
         // Variables for height and width
