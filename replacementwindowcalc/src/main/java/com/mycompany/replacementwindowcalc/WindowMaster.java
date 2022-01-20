59ddb5dac9a49ec6720f0129f0eb920b6c119f6e
HelloJon29 <jonkim1996@gmail.com>
2022-01-20 04:38:45 -0600

    Refactored code again to have 2 new methods to do input gathering and println

--- replacementwindowcalc/src/main/java/com/mycompany/replacementwindowcalc/WindowMaster.java
+++ replacementwindowcalc/src/main/java/com/mycompany/replacementwindowcalc/WindowMaster.java
@@ -9,59 +9,53 @@ import java.util.Scanner;
 public class WindowMaster {
 
     public static void main(String[] args) {
-        // Variables for height and width
-        float height;
-        float width;
-        float glass;
-        float trim;
-        float numberOfWindows;
-      
+        // Variables for height and width stored with values taken from input function
+        float height = input("Please enter window height.");
+        float width = input("Please enter window width.");
+        float glass = input("Please enter cost of the glass.");
+        float trim = input("Please enter cost of the trim.");
+        float numberOfWindows = input("How many windows?");
+
         // other variables will be used for calc values
         float areaOfWindow;
         float cost;
         float perimiterOfWindow;
         float totalWindows;
 
-        // initialize Scanner
-        Scanner myScanner = new Scanner(System.in);
-        
-        // Created a try-catch block using number format exception 
-        // to stop invalid input from user
-        try {
-            
-            // get input using the scanner and .nextFloat
-            System.out.println("Please enter window height");
-            height = myScanner.nextFloat();
-            System.out.println("Please enter window width");
-            width = myScanner.nextFloat();
-            System.out.println("Please enter the cost of the glass");
-            glass = myScanner.nextFloat();
-            System.out.println("Please enter the cost of the trim");
-            trim = myScanner.nextFloat();
-            System.out.println("How many windows?");
-            numberOfWindows = myScanner.nextFloat();
-                      
-            //calculate area of window
-            areaOfWindow = height * width;
-            //calculate perimiter of window
-            perimiterOfWindow = 2 * (height + width);
-            //calculate the total cost
-            cost = ((glass * areaOfWindow) + (trim * perimiterOfWindow));
-            totalWindows = (cost * numberOfWindows);
-            //display the results to the user
-            System.out.println("Window height = " + height);
-            System.out.println("Window width = " + width);
-            System.out.println("Window area = " + areaOfWindow);
-            System.out.println("Window perimiter = " + perimiterOfWindow);
-            System.out.println("Total Cost of single window = " + cost);
-            System.out.println("Total Cost of all windows = " + totalWindows);
-        } catch (NumberFormatException e) {
-            System.out.println("Enter only numbers!");
-        }
+        //calculate area of window
+        areaOfWindow = height * width;
+        //calculate perimiter of window
+        perimiterOfWindow = 2 * (height + width);
+        //calculate the total cost
+        cost = ((glass * areaOfWindow) + (trim * perimiterOfWindow));
+        totalWindows = (cost * numberOfWindows);
+
+        //display the results to the user after calling from results method
+        results("Window height = " + height);
+        results("Window width = " + width);
+        results("Window area = " + areaOfWindow);
+        results("Window perimiter = " + perimiterOfWindow);
+        results("Total Cost of single window = " + cost);
+        results("Total Cost of all windows = " + totalWindows);
 
     }
-    // Use this method to calculate values and return to main method
-//    public static float calcValues() {
-//        
-//    }
+
+    // Use this method to get input values and return to main method
+    public static float input(String prompt) {
+        // Iitialize Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Prompt to console
+        System.out.println(prompt);
+        // Use scanner to take input and return the value
+        float value = scanner.nextFloat();
+        return value;
+
+    }
+    // Add While loop here to catch errors instead of try-catch
+    
+    // Use this method to call println for the results in the main method
+    public static void results(String text) {
+        System.out.println(text);
+    }
 }
