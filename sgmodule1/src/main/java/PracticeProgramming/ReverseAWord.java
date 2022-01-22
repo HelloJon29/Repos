d590894b613d0959c5398ec7edd04350bf6f0369
HelloJon29 <jonkim1996@gmail.com>
2022-01-22 05:20:03 -0600

    Reverse a word excercise

---
+++ sgmodule1/src/main/java/PracticeProgramming/ReverseAWord.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticeProgramming;
+
+/**
+ *
+ * @author jonga
+ */
+public class ReverseAWord {
+    public static void main(String[] args) {
+        String word = "anime";
+        String reverse = "";
+        char r; // Had to look up char type and what methods available
+        System.out.println("The original word is " + word);
+        // for loop to increment through the length of the word String
+        for (int i = 0; i < word.length(); i++) {
+           r = word.charAt(i); // Here using the charAt method to extract each character in the word String
+           reverse = r + reverse; // Using what i learned before to add fetched iterations together
+       }
+        System.out.println("The word reversed is " + reverse);
+    }
+    
+    
+}
