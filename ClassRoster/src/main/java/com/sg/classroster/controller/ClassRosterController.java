654ec77021c1c363ecb7987156ed45ff3fdc40f1
HelloJon29 <jonkim1996@gmail.com>
2022-01-25 10:44:30 -0600

    Step 11 completed, Claster Roster project finished after implementing error handling.

--- ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
+++ ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
@@ -6,6 +6,7 @@
 package com.sg.classroster.controller;
 
 import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoException;
 import com.sg.classroster.dto.Student;
 import com.sg.classroster.ui.ClassRosterView;
 import java.util.List;
@@ -27,58 +28,62 @@ public class ClassRosterController {
     public void run() {
         boolean keepGoing = true;
         int menuSelection = 0;
-        while (keepGoing) {
-            
-            menuSelection = getMenuSelection();
-            
-            switch (menuSelection) {
-                case 1:
-                    listStudents();
-                    break;
-                case 2:
-                    createStudent();
-                    break;
-                case 3:
-                    viewStudent();
-                    break;
-                case 4:
-                    removeStudent();
-                    break;
-                case 5:
-                    keepGoing = false;
-                    break;
-                default:
-                    unknownCommand();
+        try {
+            while (keepGoing) {
+                
+                menuSelection = getMenuSelection();
+                
+                switch (menuSelection) {
+                    case 1:
+                        listStudents();
+                        break;
+                    case 2:
+                        createStudent();
+                        break;
+                    case 3:
+                        viewStudent();
+                        break;
+                    case 4:
+                        removeStudent();
+                        break;
+                    case 5:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
             }
+            exitMessage();
+        } catch (ClassRosterDaoException e) {
+            view.displayErrorMessage(e.getMessage());
         }
-        exitMessage();
-    }
-    
-    private void createStudent() {
-        view.displayCreateStudentBanner();
-        Student newStudent = view.getNewStudentInfo();
-        dao.addStudent(newStudent.getStudentId(), newStudent);
-        view.displayCreateSuccessBanner();
     }
     
     private int getMenuSelection() {
         return view.printMenuAndGetSelection();
     }
     
-    private void listStudents() {
+    private void createStudent() throws ClassRosterDaoException {
+        view.displayCreateStudentBanner();
+        Student newStudent = view.getNewStudentInfo();
+        dao.addStudent(newStudent.getStudentId(), newStudent);
+        view.displayCreateSuccessBanner();
+    }
+    
+    private void listStudents() throws ClassRosterDaoException {
         view.displayDisplayAllBanner();
         List<Student> studentList = dao.getAllStudents();
         view.displayStudentList(studentList);
     }
     
-    private void viewStudent() {
+    private void viewStudent() throws ClassRosterDaoException {
         view.displayDisplayStudentBanner();
         String studentId = view.getStudentIdChoice();
         Student student = dao.getStudent(studentId);
         view.displayStudent(student);
     }
     
-    private void removeStudent() {
+    private void removeStudent() throws ClassRosterDaoException {
         view.displayRemoveStudentBanner();
         String studentId = view.getStudentIdChoice();
         Student removedStudent = dao.removeStudent(studentId);
