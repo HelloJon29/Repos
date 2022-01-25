d0add45789cfd7acf5c37ddcdbcd477bc31813ba
HelloJon29 <jonkim1996@gmail.com>
2022-01-25 04:24:04 -0600

    Step 3 code along

--- ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
+++ ClassRoster/src/main/java/com/sg/classroster/controller/ClassRosterController.java
@@ -5,10 +5,54 @@
  */
 package com.sg.classroster.controller;
 
+import com.sg.classroster.ui.ClassRosterView;
+import com.sg.classroster.ui.UserIO;
+import com.sg.classroster.ui.UserIOConsoleImpl;
+
 /**
  *
  * @author jonga
  */
 public class ClassRosterController {
     
+    private ClassRosterView view = new ClassRosterView();
+    private UserIO io = new UserIOConsoleImpl();
+    
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            io.print("Main Menu");
+            io.print("1. List Student IDs");
+            io.print("2. Create New Student");
+            io.print("3. View a Student");
+            io.print("4. Remove a Student");
+            io.print("5. Exit");
+            
+            menuSelection = io.readInt("Please select from the" + " above choices.", 1, 5);
+            
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+        }
+        io.print("GOOD BYE");
+    }
+    
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
 }
