f27a539195dc3202706fb62b291a00587fbb18ff
HelloJon29 <jonkim1996@gmail.com>
2022-01-24 19:40:51 -0600

    Update to reverse a word

--- sgmodule1/src/main/java/PracticeProgramming/ReverseAWord.java
+++ sgmodule1/src/main/java/PracticeProgramming/ReverseAWord.java
@@ -12,13 +12,13 @@ package PracticeProgramming;
 public class ReverseAWord {
     public static void main(String[] args) {
         String word = "anime";
-        String reverse = "";
-        char r; // Had to look up char type and what methods available
+        String reverse = ""; // empty String
+        char r; // char type as empty variable to add to
         System.out.println("The original word is " + word);
         // for loop to increment through the length of the word String
         for (int i = 0; i < word.length(); i++) {
            r = word.charAt(i); // Here using the charAt method to extract each character in the word String
-           reverse = r + reverse; // Using what i learned before to add fetched iterations together
+           reverse = r + reverse; // Using what i learned before to add fetched iterations together to a empty String
        }
         System.out.println("The word reversed is " + reverse);
     }
